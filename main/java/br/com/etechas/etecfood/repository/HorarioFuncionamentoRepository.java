f897a50b6e7e51fa653264794312a9d565e5d044
Renato Luiz <no.nato051207@gmail.com>
2025-11-17 16:33:24 -0300

    Delete main/java/br/com/etechas/etecfood directory
    Signed-off-by: Renato Luiz <no.nato051207@gmail.com>

--- main/java/br/com/etechas/etecfood/repository/HorarioFuncionamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.etechas.etecfood.repository;
-
-import br.com.etechas.etecfood.entity.HorarioFuncionamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HorarioFuncionamentoRepository extends JpaRepository<HorarioFuncionamento, Long> {
-}
